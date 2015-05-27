1ff2b4c603b2e249f0bb1521c67a707e7d1e2f93
Howard <howard.suksanti@accenture.com>
2015-05-27 16:27:55 -0700

    Add Person class in test stream Java 8 feature

---
+++ Java8Lab/src/main/java/stream/Person.java
@@ -0,0 +1,35 @@
+package stream;
+
+import java.time.LocalDate;
+
+public class Person {
+
+	public enum Sex {
+		MALE, FEMALE
+	}
+
+	String _name;
+	LocalDate birthday;
+	Sex gender;
+	String emailAddress;
+	int _age;
+
+	// ...
+
+	public int getAge() {
+		// ...
+		return _age;
+	}
+	public void setAge(int age) {
+		_age = age;
+	}
+
+	public String getName() {
+		// ...
+		return _name;
+	}
+	public void setName(String name) {
+		_name = name;
+	}
+
+}
--- Java8Lab/src/main/java/stream/TestBasicStream.java
+++ Java8Lab/src/main/java/stream/TestBasicStream.java
@@ -1,5 +1,45 @@
 package stream;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestBasicStream {
+
+	public TestBasicStream() {
+		createRoster();
+	}
+
+	List<Person> group;
+
+	private void createRoster() {
+		Person rob = new Person();
+		rob.setName("Robert");
+
+		Person kevin = new Person();
+		kevin.setName("Kevin");
+
+		Person charles = new Person();
+		charles.setName("Charles");
+
+		group = new ArrayList<Person>();
+		group.add(rob);
+		group.add(kevin);
+		group.add(charles);
+
+	}
+
+	// test print all with foreach
+	public void printAllForEach() {
+
+		for (Person p : group) {
+			System.out.println(p.getName() + " foreach ");
+		}
+	}
+
+	// test with new Java 8 stream
+
+	public static void main(String[] args) {
+
+	}
 
 }
