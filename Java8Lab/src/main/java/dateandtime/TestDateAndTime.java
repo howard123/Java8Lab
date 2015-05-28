15eb6b4d5695b27d3c5bae3193e8bc11e0d60f9a
Howard <howard.suksanti@accenture.com>
2015-05-28 15:27:29 -0700

    add Date time class

---
+++ Java8Lab/src/main/java/dateandtime/TestDateAndTime.java
@@ -0,0 +1,16 @@
+package dateandtime;
+
+import java.time.LocalDateTime;
+
+public class TestDateAndTime {
+
+	public void printLocalDate() {
+		LocalDateTime ldt = LocalDateTime.now();
+
+		System.out.println(ldt);
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
