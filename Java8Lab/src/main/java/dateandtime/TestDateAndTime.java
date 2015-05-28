94b9069cf5c482f4a0e7123cf91e97c1d7c03964
Howard <howard.suksanti@accenture.com>
2015-05-28 15:39:00 -0700

    add method in LocalDateTime class

--- Java8Lab/src/main/java/dateandtime/TestDateAndTime.java
+++ Java8Lab/src/main/java/dateandtime/TestDateAndTime.java
@@ -1,16 +1,42 @@
 package dateandtime;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
+/**
+ * This class is for testing java localdatetime. The new class improve on type
+ * safe. This new class is immutible(thread-safe).
+ * 
+ * @author howard.suksanti
+ *
+ */
 public class TestDateAndTime {
 
-	public void printLocalDate() {
-		LocalDateTime ldt = LocalDateTime.now();
+	LocalDateTime ldt = LocalDateTime.now();
 
+	public void printLocalDate() {
 		System.out.println(ldt);
 	}
 
-	public static void main(String[] args) {
+	public void printSecond() {
+		System.out.println(ldt.getSecond());
+	}
 
+	public void printPlusWeek() {
+		System.out.println(ldt.plusWeeks(1));
+	}
+
+	public void printTruncateSecond() {
+		System.out.println(ldt.truncatedTo(ChronoUnit.SECONDS));
+
+	}
+
+	public static void main(String[] args) {
+		TestDateAndTime td = new TestDateAndTime();
+
+		td.printLocalDate();
+		td.printSecond();
+		td.printPlusWeek();
+		td.printTruncateSecond();
 	}
 }
